42fb602a0dc21b0d1766bfc8ca021eb03eaac670
RAND ALHASSOUN <rialhassoun22@twq.idserve.net>
2023-02-07 11:07:41 +0300

    Initial Commit

---
+++ BaheyApp/BaheyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaheyAppApp.swift
+//  BaheyApp
+//
+//  Created by Rand Alhassoun on 07/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct BaheyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
