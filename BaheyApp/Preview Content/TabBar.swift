00e84ab8011a52ddcb355c8c67a32e7248d9ea51
RAND ALHASSOUN <rialhassoun22@twq.idserve.net>
2023-02-08 09:39:47 +0300

    ,

---
+++ BaheyApp/Preview Content/TabBar.swift
@@ -0,0 +1,20 @@
+//
+//  TabBar.swift
+//  BaheyApp
+//
+//  Created by Rand Alhassoun on 08/02/2023.
+//
+
+import SwiftUI
+
+struct TabBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TabBar_Previews: PreviewProvider {
+    static var previews: some View {
+        TabBar()
+    }
+}
