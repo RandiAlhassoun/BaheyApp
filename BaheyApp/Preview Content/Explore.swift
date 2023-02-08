6f989f71c425005625b1e5dd0ca84dca243816d2
norahm224 <norah.213113778@gmail.com>
2023-02-08 13:03:25 +0300

    change explore

--- BaheyApp/Preview Content/Explore.swift
+++ BaheyApp/Preview Content/Explore.swift
@@ -8,13 +8,67 @@
 import SwiftUI
 
 struct Explore: View {
+    @State private var searchText = ""
+    let names = ["Holly", "Josh", "Rhonda", "Ted"]
+    
     var body: some View {
-        Circle().foregroundColor(Color("Lgreen"))
+        VStack {
+            
+        Text("المتخصصات")
+        NavigationStack {
+            List {
+                ForEach(searchResults, id: \.self) { name in
+                    NavigationLink {
+                        Text(name)
+                    } label: {
+                        Text(name)
+                    }
+                }
+            }
+  
+        }
+        .searchable(text: $searchText)
     }
 }
+    var searchResults: [String] {
+          if searchText.isEmpty {
+              return names
+          } else {
+              return names.filter { $0.contains(searchText) }
+          }
+      }
+    }
+
 
 struct Explore_Previews: PreviewProvider {
     static var previews: some View {
         Explore()
+    }
+}
+
+//Build service provider frame
+struct ServiceCerd: View {
+//    var folder: MyNotes
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .fill(Color.white)
+                .shadow(radius: 4)
+            
+                .frame(width: 317, height: 74)
+                .overlay(
+                 //   Text(folder.name)
+                    Text("")
+                        .font(.title3)
+                        .bold()
+                )
+            Rectangle()
+         
+                .frame(width: 20, height: 74)
+                .padding(.trailing,299)
+              //  .foregroundColor(folder.color)
+                .foregroundColor(Color.blue)
+        }
+    
     }
 }
---
+++ BaheyApp/Preview Content/New Group/Service Provider.swift
@@ -0,0 +1,29 @@
+//
+//  Service Provider.swift
+//  BaheyApp
+//
+//  Created by Nourah Almusaad on 08/02/2023.
+//
+
+import Foundation
+//import SwiftUI
+
+
+//class MyNotesViewModel: ObservableObject {
+//    @Published var folders = MyNotes.testFolder
+//}
+//
+//struct MyNotes: Identifiable {
+//    var id = UUID()
+//   var color: Color
+//    var name: String
+//}
+//
+//extension MyNotes {
+//    static let testFolder = [
+//        MyNotes( color: Color("Pink"), name: NSLocalizedString("Work", comment: "")),
+//        MyNotes( color: Color("Yellow"), name: NSLocalizedString("House furniture", comment: "")),
+//        MyNotes(color: Color("Green"), name:  NSLocalizedString("Spending", comment: ""))
+//    ]
+//
+//}
